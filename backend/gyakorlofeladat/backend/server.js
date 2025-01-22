fec236149e36cc187a581b38f97c4b46ab110cdb
Majzik "Mini Pablo" Bence <majben686@hengersor.hu>
2025-01-22 10:19:25 +0100

    2025_01_22
    gyakorlofeladatok

---
+++ backend/gyakorlofeladat/backend/server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mysql = require("mysql");
+const bodyParser = require("body-parser");
+app .use(bodyParser.json());
+
+const db = mysql.createConnection(
+    {
+        user: "root",
+        host: "localhost",
+        password: "",
+        database: "kozutak",
+    }
+);
